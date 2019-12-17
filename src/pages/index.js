c84767748dbde65ed28cd109284ddd026cb708e7
Vladyslav Konovalchuk <berlitio600@gmail.com>
2019-12-17 22:32:34 +0200

    Update index page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,21 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Layout from "../components/Layout"
+import MainBanner from "../components/MainBanner"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
 
-export default () => <Layout>Tour Site</Layout>
+export default () => (
+  <Layout>
+    <MainBanner>
+      <Banner
+        title="Awesome Api0"
+        info="Come and Explore Hapi, the city of ruins, which is a UNESCO World Heritage Site"
+      >
+        <Link className="btn-white" to="/places">
+          expolore places
+        </Link>
+      </Banner>
+    </MainBanner>
+  </Layout>
+)
