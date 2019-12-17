aaa9edd7541f82bf364399937120fcc2df355455
Vladyslav Konovalchuk <berlitio600@gmail.com>
2019-12-17 22:32:04 +0200

    Added content to eror page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
 
 export default function errorPage() {
-  return <Layout>Error page</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops, something wrong">
+          <Link to="/" >back to home page</Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
